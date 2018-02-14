a96d14a4abc17cd7d4e48cabb8df32e0bcf60f5c
Team BRJ <zamiur53@gmail.com>
2018-02-14 12:42:06 +0600

    1. Material design theme for full screen is not working .....    i changed the app theme....    App crash bug fixed....... 2. Button and Image obj created... 3. Image array created. 4. Random method added for selecting image randomly... 5. Onclicklistener method is added for button action.......

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button askButton = (Button) findViewById(R.id.buttonAsk);
+        final ImageView viewImage = (ImageView) findViewById(R.id.imageView);
+
+
+        final int[] imageArray = {R.drawable.ball1_2x,
+                R.drawable.ball2_2x,
+                R.drawable.ball3_2x,
+                R.drawable.ball4_2x,
+                R.drawable.ball5_2x};
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomImage = new Random();
+                int number =randomImage.nextInt(5);
+
+                viewImage.setImageResource(imageArray[number]);
+
+
+            }
+        });
+
     }
 }
